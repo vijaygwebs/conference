0400585d02d014d6a11d359fed0445e9b7d57cd7
vijaygwebs <vijay.gwebs@gmail.com>
2018-10-12 15:40:01 +0530

    first commit to fronend branch

--- _protected/frontend/views/site/index.php
+++ _protected/frontend/views/site/index.php
@@ -3,65 +3,141 @@
 /* @var $this yii\web\View */
 $this->title = Yii::t('app', Yii::$app->name);
 ?>
-<div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully installed Yii2 improved application template</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.freetuts.org/tutorial/view?id=6">Read our tutorial</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-3">
-                <h3>Yii documentation</h3>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-3">
-                <h3>Yii forum</h3>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-3">
-                <h3>Yii extensions</h3>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-            <div class="col-lg-3">
-                <h3>Freetuts.org</h3>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.freetuts.org/">Freetuts.org &raquo;</a></p>
+<div class="container">
+    <div class="row">
+        <div class="content">
+            <div class="col-md-12">
+                <div class="head2">
+                    <h2>WorkBench</h2>
+                </div>
+                <p class="pg">EasyChair is a conference management system that is flexible, easy to use, and has many features to make it suitable for various conference models. It is currently probably the most commonly used conference management system. If you would like to know why you should use EasyChair for your conference, look at this image!</p>
+                <div class="head2">
+                    <h2>What WorkBench Helps You to Do</h2>
+                </div>
+                <p class="pg">EasyChair was designed to help conference organisers to cope with the complexity of the refereeing process. The current version supports</p>
+                <ul class="content-ul">
+                    <li><i class="fa fa-angle-double-right"></i>Management and monitoring of the program committee.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Sophisticated and flexible management of the access of PC automatic paper submission.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Automatic paper submission.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Paper assignment based on the preferences of PC members.</li>
+                    <li><i class="fa fa-angle-double-right"></i>List of the latest events.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Submission of reviews.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Sending email to PC members, referees and authors.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Monitoring email.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Online discussion of papers.</li>
+                    <li><i class="fa fa-angle-double-right"></i>The authoar response (aka rebuttal) phase, when the author can respond to the reviews.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Preparation of conference proceedings.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Generation and publishing of conference program.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Generation of conference broshures with program.</li>
+                    <li><i class="fa fa-angle-double-right"></i>Conference program and proceedings on flash drives.</li>
+                </ul>
+                <div class="head2">
+                    <h2>Supported Models</h2>
+                </div>
+                <p class="pg">Currently, EasyChair supports two models.</p>
+                <ul class="content-ul full">
+                    <li><i class="fa fa-angle-double-right"></i>The standard model is intended for conferences having a single program committee. The papers are distributed among PC members, normally based on their preferences.</li>
+                    <li><i class="fa fa-angle-double-right"></i>The multi-track version is for conferences having multiple tracks. Each track has its own program committee and one of more track chairs. This model requires a "superchair" to monitor or supervise the work of the tracks.</li>
+                </ul>
+                <p class="pg">For each of t  he supported models the users can choose among various options to adjust the system to their requirements. For example, they can choose to use anonymous submissions.</p>
             </div>
         </div>
-
     </div>
 </div>
+<div class="brown-box">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="head2">
+                    <h2>What WorkBench Helps You to Do</h2>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="help-item">
+                    <img src="images/icon1.png">
+                    <h3>molecular research</h3>
+                    <p>A material is defined as a substance (most often a solid,but other condensed phases can be included)that is intended to be used for certain applications.</p>
+                    <a class href="#">read more</a>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="help-item">
+                    <img src="images/icon2.png">
+                    <h3>Dedicated Team</h3>
+                    <p>A material is defined as a substance (most often a solid,but other condensed phases can be included)that is intended to be used for certain applications.</p>
+                    <a class href="#">read more</a>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="help-item">
+                    <img src="images/icon3.png">
+                    <h3>Latest Standards</h3>
+                    <p>A material is defined as a substance (most often a solid,but other condensed phases can be included)that is intended to be used for certain applications.</p>
+                    <a class href="#">read more</a>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="help-item">
+                    <img src="images/icon4.png">
+                    <h3>Green Energy</h3>
+                    <p>A material is defined as a substance (most often a solid,but other condensed phases can be included)that is intended to be used for certain applications.</p>
+                    <a class href="#">read more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="content">
+            <div class="col-md-12">
+                <div class="head2">
+                    <h2>WorkBench Installation</h2>
+                </div>
+                <p class="pg">Installing EasyChair for your conference is easy. You do not have to download or configure software or arrange backup procedures yourself. All you have to do is to fill out a simple form. The reason for this is that we host all conferences on our server and use our own backup, recovery, mail sending and other procedure. As far as the users are concerned, EasyChair is simply a Web service.</p>
+                <p class="pg">If you want to use EasyChair for managing a conference, you should request an inastallation first. This page describes the new conference installation process of EasyChair. It consists of the following steps. </p>
+                <div class="tab-box">
+                    <div class="tab-left">
+                        <a class="active">Our Policy</a>
+                        <a>Workbench Account</a>
+                        <a>Request</a>
+                        <a>Verification</a>
+                        <a>Further Processing</a>
+                        <a>Initialization</a>
+                    </div>
+                    <div class="tab-right">
+                        <div style="display:block;">
+                            <p class="pg">When we verified your request, you will receive a notification by email. From this moment on all information about processing your request will also be available under the menu tabs EasyChair -> My installation requests. The majority of our requests are granted. However, if we have insufficient information we may ask you some questions about your conference. You should check your mailbox for a notification from us or log in to EasyChair and view the status of your request.</p>
+                        </div>
+                        <div>
+                            <p class="pg">We check the information you specified. We have to do this for several reasons: security, to avoid multiple installations for the same conference and to make sure that you did not fill out the form by mistake. The verification process can take anything between a minute and 12 hours. On the average it takes about 2 hours. Normally, requests made during the night time in the UK are processed slower.</p>
+                            <p class="pg">When we verified your request, you will receive a notification by email. From this moment on all information about processing your request will also be available under the menu tabs EasyChair -> My installation requests. The majority of our requests are granted. However, if we have insufficient information we may ask you some questions about your conference. You should check your mailbox for a notification from us or log in to EasyChair and view the status of your request.</p>
+                        </div>
+                        <div>
+                            <p class="pg">When we verified your request, you will receive a notification by email. From this moment on all information about processing your request will also be available under the menu tabs EasyChair -> My installation requests. The majority of our requests are granted. However, if we have insufficient information we may ask you some questions about your conference. You should check your mailbox for a notification from us or log in to EasyChair and view the status of your request.</p>
+                        </div>
+                        <div>
+                            <p class="pg">We check the information you specified. We have to do this for several reasons: security, to avoid multiple installations for the same conference and to make sure that you did not fill out the form by mistake. The verification process can take anything between a minute and 12 hours. On the average it takes about 2 hours. Normally, requests made during the night time in the UK are processed slower.</p>
+                            <p class="pg">When we verified your request, you will receive a notification by email. From this moment on all information about processing your request will also be available under the menu tabs EasyChair -> My installation requests. The majority of our requests are granted. However, if we have insufficient information we may ask you some questions about your conference. You should check your mailbox for a notification from us or log in to EasyChair and view the status of your request.</p>
+                        </div>
+                        <div>
+                            <p class="pg">When we verified your request, you will receive a notification by email. From this moment on all information about processing your request will also be available under the menu tabs EasyChair -> My installation requests. The majority of our requests are granted. However, if we have insufficient information we may ask you some questions about your conference. You should check your mailbox for a notification from us or log in to EasyChair and view the status of your request.</p>
+                        </div>
+                        <div>
+                            <p class="pg">We check the information you specified. We have to do this for several reasons: security, to avoid multiple installations for the same conference and to make sure that you did not fill out the form by mistake. The verification process can take anything between a minute and 12 hours. On the average it takes about 2 hours. Normally, requests made during the night time in the UK are processed slower.</p>
+                            <p class="pg">When we verified your request, you will receive a notification by email. From this moment on all information about processing your request will also be available under the menu tabs EasyChair -> My installation requests. The majority of our requests are granted. However, if we have insufficient information we may ask you some questions about your conference. You should check your mailbox for a notification from us or log in to EasyChair and view the status of your request.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="head2">
+                    <h2>Licenses</h2>
+                </div>
+                <p class="pg">EasyChair is available under a number of licenses, including a free one, see the license page for details.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
